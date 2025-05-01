a756b9e5d8e00f708852a9b8f6ce1d64a7100fea
Dalton Kajander <daltonkajander@yahoo.com>
2025-05-01 11:52:01 -0400

    Added recipes and changed from List to Dictionary

--- main.py
+++ main.py
@@ -1,45 +1,5 @@
-class Inventory():
-    items: list
-    
-    def __init__(self, starting: list = None) -> None: 
-        self.items = []
-        
-        if (starting is not None):
-            for item in starting:
-                if (isinstance(item, Ingredient)): self.items.append(item)
-                elif (isinstance(item, Inventory)): self.items.extend(item.items)
-                else: raise TypeError(f"Expected Ingredient or Inventory, got {type(item).__name__}")
-     
-    
-    def add(self, other) -> None:
-        if (isinstance(other, Inventory)):  self.items += other.items
-        elif (isinstance(other, Ingredient)):  self.items.append(other)
-        else: raise TypeError(f"Expected Ingredient or Inventory, got {type(other).__name__}")
-        self.combine_items()
-        
-    def has_item(self, other) -> bool:
-        if (isinstance(other, Ingredient)): name = other.name
-        elif (isinstance(other, str)): name = other
-        else: raise TypeError(f"Expected Ingredient or Inventory, got {type(other).__name__}")
-        return any(item.name == name and item.amount >= 1 for item in self.items)
-    
-    def combine_items(self):
-        ...
-        
-        
-        
-            
+import json
 
-class Recipe():
-    inputs: list
-    outputs: list
-    station: str
-    
-    def __init__(self, inputs: list, outputs: list, station: str = None):
-        self.inputs = inputs
-        self.outputs = outputs
-        self.station = station
-        
 class Ingredient():
     name: str
     amount: int
@@ -54,6 +14,10 @@ class Ingredient():
         if (not isinstance(other, Ingredient)): return NotImplemented
         elif (self.name != other.name): raise ValueError(f"Cannot add Ingredients of diffent types, {self.name} and {other.name}")
         return Ingredient(self.name, self.amount + other.amount)
+    def __mult__(self, other):
+        if (not isinstance(other, int)): return NotImplemented
+        elif (other < 0): raise ValueError(f"Expected non-negative integer, got {other}")
+        return Ingredient(self.name, self.amount * other)
     
     def __sub__(self, other):
         if (not isinstance(other, Ingredient)): return NotImplemented
@@ -61,17 +25,92 @@ class Ingredient():
         elif (self.amount < other.amount): raise ValueError(f"Cannot have negative amounts of ingredients")
         return Ingredient(self.name, self.amount - other.amount)
 
-inventory = Inventory()
+class Recipe():
+    inputs: list
+    outputs: list
+    station: str
+    
+    def __init__(self, inputs: list, outputs: list, station: str = "Crafting Table"):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.station = station
+        
+    def __str__(self):
+        s = f"[{self.station}]\nInputs:"
+        for item in self.inputs:
+            s += f"\n  - {item}"
+        s += "\nOutputs:"
+        for item in self.outputs:
+            s += f"\n  - {item}"
+        return s
 
+class Inventory():
+    items: dict
+    
+    def __init__(self, starting: dict = None) -> None: 
+        self.items = {}
+        
+        if (starting is not None):
+            for item_name in starting:
+                if (not isinstance(starting[item_name], Ingredient)): 
+                    raise TypeError(f"Expected Ingredient, got {type(starting[item_name]).__name__}")
+            self.items = starting
+     
+    
+    def add(self, other) -> None:
+        # Add an item
+        if (isinstance(other, Ingredient)):
+            if (other.name in self.items): self.items[other.name] += other
+            else: self.items[other.name] = other
+        # Add an inventory
+        elif (isinstance(other, Inventory)):
+            for item in other.items:
+                self.add(other.items[item])
+        # Failed to add
+        else:
+            raise TypeError(f"Expected Ingredient or Inventory, got {type(other).__name__}")
+        
+    def has_item(self, other) -> bool:
+        if (isinstance(other, Ingredient)): name = other.name
+        elif (isinstance(other, str)): name = other
+        else: raise TypeError(f"Expected Ingredient or str, got {type(other).__name__}")
+        return name in self.items
+    
+    def has_amount(self, other, amount = None) -> bool:
+        if (not self.has_item(other)): return False
+        
+        if (isinstance(other, Ingredient)): 
+            name = other.name
+            check_amount = other.amount
+        elif (isinstance(other, str) and isinstance(amount, int)):
+            name = other
+            check_amount = amount
+        else: raise TypeError(f"Expected int for amount, got {type(amount).__name__}")
+        
+        return self.items[name].amount >= check_amount
+        
+    def can_craft(self, recipe: Recipe) -> bool:
+        for item in recipe.inputs:
+            if (not self.has_amount(item)): return False
+        return True
+    
+    def __str__(self):
+        s = "Inventory:"
+        for i in self.items:
+            s += f"\n  - {i}: {self.items[i].amount}"
+        s += "\n  - Empty" if len(self.items) == 0 else ""
+        return s
+        
+def read_recipes(filename) -> list:
+    with open(filename, "r") as file:
+        ...
 
-wood_log = Ingredient("Wood Log")
-wood_plank = Ingredient("Wood Plank")
-cobblestone = Ingredient("Cobblestone")
-redstone_dust = Ingredient("Redstone Dust")
-iron_ore = Ingredient("iron_ore")
-iron_ingot = Ingredient("Iron Ingot")
+piston_recipe = Recipe([Ingredient("Wood Plank", 3), Ingredient("Cobblestone", 4), Ingredient("Redstone Dust", 1), Ingredient("Iron Ingot", 1)], [Ingredient("Piston", 1)])
 
-a = [1,2,3]
-b = [4,5]
+inv = Inventory()
+inv.add(Ingredient("Wood Plank", 3))
+inv.add(Ingredient("Cobblestone", 4))
+inv.add(Ingredient("Redstone Dust", 0))
+inv.add(Ingredient("Iron Ingot", 1))
 
-print(a + b)
+print(inv.can_craft(piston_recipe))
